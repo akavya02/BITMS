96c2fdb0be121ea9700148770975be53fddd5a9a
akavya02 <167055362+akavya02@users.noreply.github.com>
2024-05-01 21:45:48 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,22 @@
+'''s="abcdefgh"
+s=s+'0'
+print(s)'''
+
+'''s="hfnmmgnbg"
+print(s[-3])'''
+'''s="abcdefgh"
+print(s[7:1:-1])'''
+
+'''s="4python p"
+print(s.isupper())
+print(s.islower())
+print(s.isalpha())
+print(s.isalnum())
+print(s.isspace())'''
+'''s="4python programming"
+print(s.split("n"))
+print(s.split())
+print(s.partition)'''
+a=['1','2','3']
+a=" ".join(a)
+print(a,type(a),a[0])
