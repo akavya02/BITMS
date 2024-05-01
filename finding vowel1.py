b66ebb64e40f0759a64447d1b26176873a58a4c8
akavya02 <167055362+akavya02@users.noreply.github.com>
2024-05-01 21:34:36 +0530

    Add files via upload

---
+++ finding vowel1.py
@@ -0,0 +1,14 @@
+c=input()
+match c:
+    case 'a':
+        print("a is vowel")
+    case 'e':
+        print("e is vowel")
+    case 'i':
+        print("i is vowel")
+    case'o':
+        print("o is vowel")
+    case'u':
+        print("u is vowel")
+    case _:
+        print("not a vowel")
